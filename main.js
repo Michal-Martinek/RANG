e6661b2750b9c25f1758c51dc18f77e88b91e9d0
Michal Martínek <michal7952@seznam.cz>
2024-02-04 13:17:37 +0100

    Added button for multiple generations

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 
-function updateName() {
-	let elem = document.getElementById('airlineName')
-	elem.innerHTML = generateName()
+var nameCount = 5;
+
+function updateNames() {
+	let elem = document.getElementById('airline-names')
+	elem.innerHTML = ''
+	for (let i = 0; i < nameCount; ++i) {
+		let e = document.createElement('p')
+		e.innerText = generateName()
+		elem.appendChild(e)
+	}
+}
+function countButtonClick() {
+	let btn = document.getElementById('count-btn')
+	if (nameCount == 1) {
+		btn.innerHTML = 'Less'
+		nameCount = 5;
+	} else {
+		btn.innerHTML = 'More'
+		nameCount = 1;
+	}
+	updateNames()
 }
 function main() {
-	updateName()
+	updateNames()
 }
