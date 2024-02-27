5239eb5b22a880f6e882f62e2da4263ac7491d45
Michal Martínek <michal7952@seznam.cz>
2024-02-27 21:38:13 +0100

    Added new templates

--- templates.js
+++ templates.js
@@ -9,6 +9,12 @@ grammar.template = [
 	'Aer[o][ ](adjective)',
 	'Air[ ](adjective)',
 	'{Avia}(adjective){avia}',
+	'(adjective) express',
+	'(adjective) flight',
+	'{Cargo} (adjective) {cargo}',
+	'(adjective) link',
+	'(adjective) connector',
+	'(adjective) wind',
 ];
 
 grammar.adjective = ['(material)', '(number)', '(state)', '(collection)']
